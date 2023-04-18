de8a93f11d2ffa68883c8b611137e8e14d308f39
Bailey P Forbes <62630945+BPForbes@users.noreply.github.com>
2023-04-18 17:21:08 -0400

    Update TipController.java

--- c202-18293-sp23-lab9-BPForbes-main/src/main/java/com/example/javafxlab/TipController.java
+++ c202-18293-sp23-lab9-BPForbes-main/src/main/java/com/example/javafxlab/TipController.java
@@ -8,18 +8,37 @@ public class TipController {
     @FXML
     private TextField total;
 
-    // Several more things to bind to the UI here
+    @FXML
+    private ChoiceBox<Service> choiceBox;
 
     @FXML
     public void initialize() {
+        choiceBox.getItems().addAll(
+            Service.NO_TIP,
+            Service.CASH,
+            Service.POOR,
+            Service.GOOD,
+            Service.EXCELLENT,
+            Service.GREAT
+        );
+        choiceBox.setValue(Service.GOOD);
         total.setEditable(false);
-        // More here
     }
 
     @FXML
     protected void onCalculateClick() {
         try {
-            // Get the bill and calculate here
+            // Get the bill and service
+            double bill = Double.parseDouble(total.getText());
+            Service service = choiceBox.getValue();
+
+            // Calculate the tip and total amount
+            double tip = service.tip();
+            double totalAmount = bill + tip;
+
+            // Display the tip and total amount
+            tipLabel.setText("$" + tip);
+            totalLabel.setText("$" + totalAmount);
         } catch (NumberFormatException ex) {
             total.setText("Enter a valid bill amount");
         }
